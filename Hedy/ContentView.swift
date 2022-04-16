1d92147c281ad2ab06b26d7f141c184d9bf0a72d
Ferdia McKeogh <ferdia@mckeogh.tech>
2022-04-16 16:28:36 +0100

    Initial Commit

---
+++ Hedy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hedy
+//
+//  Created by Ferdia McKeogh on 16/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hedy/HedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HedyApp.swift
+//  Hedy
+//
+//  Created by Ferdia McKeogh on 16/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
